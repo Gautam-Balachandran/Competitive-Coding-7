2cba9846a2e486e4ca522cf29665c8ac627c056a
Gautam-Balachandran <gautam.balachandran@gmail.com>
2024-07-01 15:23:28 -0400

    Competitive-Coding-7 Completed

---
+++ KthSmallestInSortedMatrix.py
@@ -0,0 +1,49 @@
+﻿# Time Complexity : O(klongn)
+# Space Complexity : O(n)
+
+import heapq
+
+class Solution:
+    def kthSmallest(self, matrix, k):
+        n = len(matrix)
+        min_heap = []
+
+        # Initialize the heap with the first element of each row
+        for row in range(n):
+            heapq.heappush(min_heap, (matrix[row][0], row, 0))
+
+        # Extract-min k times
+        small = 0
+        for _ in range(k):
+            small, row, col = heapq.heappop(min_heap)
+
+            # If there are more elements in the row, add the next element to the heap
+            if col + 1 < n:
+                heapq.heappush(min_heap, (matrix[row][col + 1], row, col + 1))
+
+        return small
+
+# Examples
+solution = Solution()
+
+matrix1 = [
+    [1, 5, 9],
+    [10, 11, 13],
+    [12, 13, 15]
+]
+k1 = 8
+print(solution.kthSmallest(matrix1, k1)) # Expected output: 13
+
+matrix2 = [
+    [1, 2],
+    [1, 3]
+]
+k2 = 3
+print(solution.kthSmallest(matrix2, k2)) # Expected output: 2
+
+matrix3 = [
+    [-5, -4],
+    [-3, -1]
+]
+k3 = 2
+print(solution.kthSmallest(matrix3, k3)) # Expected output: -4
---
+++ MeetingRoomsII.py
@@ -0,0 +1,35 @@
+﻿# Time Complexity : O(nlogn)
+# Space Complexity : O(n)
+
+from collections import defaultdict
+
+class MeetingRoomsII:
+    def minMeetingRooms(self, intervals):
+        if not intervals:
+            return 0
+
+        timeline = defaultdict(int)
+        room_count = 0
+        cur = 0
+
+        for interval in intervals:
+            start, end = interval
+            timeline[start] += 1
+            timeline[end] -= 1
+
+        for time in sorted(timeline.keys()):
+            cur += timeline[time]
+            room_count = max(room_count, cur)
+
+        return room_count
+
+# Examples
+solution = MeetingRoomsII()
+
+intervals1 = [[0, 30], [5, 10], [15, 20]]
+intervals2 = [[7, 10], [2, 4]]
+intervals3 = [[1, 5], [2, 6], [8, 9], [8, 9]]
+
+print(solution.minMeetingRooms(intervals1)) # Output: 2
+print(solution.minMeetingRooms(intervals2)) # Output: 1
+print(solution.minMeetingRooms(intervals3)) # Output: 2
